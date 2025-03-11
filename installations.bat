13a5931b8917de6edf32172cb8c2cbd1a6b9839e
sedlacekjt <73354043+sedlacekjt@users.noreply.github.com>
2025-03-10 23:22:13 -0700

    Added initial bat file for pc installation
    Update installations.bat
    
    Removed SteamCMD from the installations list
    
    Update installations.bat
    
    Fixed misspelling
    
    Organized and added some applications
    
    Organized and added some optional applications
    
    Update installations.bat

---
+++ installations.bat
@@ -0,0 +1,28 @@
+winget install --accept-package-agreements --accept-source-agreements --id Microsoft.PowerShell
+winget install --accept-package-agreements --accept-source-agreements --id 7zip.7zip
+winget install --accept-package-agreements --accept-source-agreements --id Google.Chrome
+winget install --accept-package-agreements --accept-source-agreements --id Adobe.Acrobat.Reader.64-bit
+winget install --accept-package-agreements --accept-source-agreements --id Blizzard.BattleNet
+winget install --accept-package-agreements --accept-source-agreements --id Discord.Discord
+winget install --accept-package-agreements --accept-source-agreements --id Dropbox.Dropbox
+winget install --accept-package-agreements --accept-source-agreements --id EpicGames.EpicGamesLauncher
+winget install --accept-package-agreements --accept-source-agreements --id Git.Git
+winget install --accept-package-agreements --accept-source-agreements --id Google.GoogleDrive
+winget install --accept-package-agreements --accept-source-agreements --id Grammarly.Grammarly
+winget install --accept-package-agreements --accept-source-agreements --id Logitech.GHUB
+winget install --accept-package-agreements --accept-source-agreements --id Loupedeck.Loupedeck
+winget install --accept-package-agreements --accept-source-agreements --id Microsoft.VisualStudioCode
+winget install --accept-package-agreements --accept-source-agreements --id OBSProject.OBSStudio
+winget install --accept-package-agreements --accept-source-agreements --id Oracle.VirtualBox
+winget install --accept-package-agreements --accept-source-agreements --id RazerInc.RazerInstaller4
+winget install --accept-package-agreements --accept-source-agreements --id Spotify.Spotify
+winget install --accept-package-agreements --accept-source-agreements --id Valve.Steam
+winget install --accept-package-agreements --accept-source-agreements --id Ultimaker.Cura
+winget install --accept-package-agreements --accept-source-agreements --id VideoLAN.VLC
+winget install --accept-package-agreements --accept-source-agreements --id Zoom.Zoom
+
+# GPU-Specific Software is not supported by winget
+
+# Optional for troubleshooting and testing purposes
+# winget install --accept-package-agreements --accept-source-agreements --id AntibodySoftware.WizTree
+# winget install --accept-package-agreements --accept-source-agreements --id REALiX.HWiNFO
